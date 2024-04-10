33d17963d7204ca436579de1b25d747440590107
Kathryn <kathrynpeters17@gmail.com>
2024-04-10 15:42:28 -0400

    My message

---
+++ knp_main.py
@@ -0,0 +1,33 @@
+#Lab 09 Kathryn Peters
+def encode(password):
+    encoded_pw = ""
+    for i in password:
+        new = int(i)+3
+        encoded_pw += str(new)
+
+    return encoded_pw
+
+# print(encode("12345"))
+
+def main():
+    choose = 0
+    while choose != 3:
+        #Print menu
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        choose = int(input("\nPlease enter an option: "))
+
+        if choose == 1:
+            password = input("Please enter your password to encode: ")
+            encode(password)
+            print("Your password has been encoded and stored!\n")
+
+        elif choose == 2:
+            pw = encode(password)
+            print(f"The encoded password is {pw} and the original password is {password}")
+
+if __name__ == "__main__":
+    main()
