a464f6d21c789943927386fbc27348cbb1ea92ce
Sabrina Joseph <sab@Sabs-Mac.local>
2024-04-10 16:55:04 -0400

    Final Draft

--- knp_main.py
+++ knp_main.py
@@ -8,7 +8,13 @@ def encode(password):
     return encoded_pw
 
 # print(encode("12345"))
-
+# Sabrina Is Here!
+def decoder(encoded_pw):
+    decoded_pw = ''
+    for num in encoded_pw:
+        decoded_digit = str(int(num) - 3)  # Shifting each digit down by 3
+        decoded_pw += decoded_digit
+    return decoded_pw
 def main():
     choose = 0
     while choose != 3:
@@ -26,8 +32,7 @@ def main():
             print("Your password has been encoded and stored!\n")
 
         elif choose == 2:
-            pw = encode(password)
-            print(f"The encoded password is {pw} and the original password is {password}")
+            print(f"The encoded password is {encode(password)}, and the original password is {decoder(encode(password))}.\n")
 
 if __name__ == "__main__":
     main()
